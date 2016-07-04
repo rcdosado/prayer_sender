b9aa65ede771c0bff30e5b3d74fe25c6faac5f50
roy <rcdosado@gmail.com>
2016-07-04 23:42:40 +0800

    add code to generate random IP address

---
+++ sender.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+# SUMMARY:
+# This script, sends a prayer to random IP addresses
+# prayer is scrape from another website.
+# Coded by rcd
+
+import socket
+import sys
+import random
+
+#generate random IP addresses
+host = socket.inet_ntoa(struct.pack('>I', random.randint(1,0xffffffff)))
+
+try:
+   s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+except:
+   print "socket() failed"
+   sys.exit(1)
+
+
